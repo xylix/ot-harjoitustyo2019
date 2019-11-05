c868c671925c374ecd56ee1eedcf17224521d781
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-11-05 18:49:14 +0200

    Add excercise tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoOikein() {
+        assertEquals(10, kortti.saldo());      
+    }
+    
+    @Test
+    public void lataaminen() {
+        int vanhaSaldo = kortti.saldo();
+        kortti.lataaRahaa(10);
+        assertEquals(vanhaSaldo + 10, kortti.saldo());
+    }
+    
+    @Test
+    public void veloitus() {
+        int vanhaSaldo = kortti.saldo();
+        kortti.otaRahaa(5);
+        assertEquals(vanhaSaldo - 5, kortti.saldo());
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void yliVeloitus() {
+        int vanhaSaldo = kortti.saldo();
+        kortti.otaRahaa(15);
+        assertEquals(vanhaSaldo, kortti.saldo());
+    }
+    
+    @Test
+    public void veloitusPalautusArvoTesti() {
+        assertTrue(kortti.otaRahaa(9));
+        assertFalse(kortti.otaRahaa(20));
+    }
 }
