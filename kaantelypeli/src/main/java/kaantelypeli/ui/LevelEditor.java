6ce20f196845d409eb43a40bbe210d803cc478a2
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-12-16 22:37:15 +0200

    Change LevelEditor type selection to dialog box

--- kaantelypeli/src/main/java/kaantelypeli/ui/LevelEditor.java
+++ kaantelypeli/src/main/java/kaantelypeli/ui/LevelEditor.java
@@ -1,10 +1,8 @@
 package kaantelypeli.ui;
 
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceDialog;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -15,12 +13,13 @@ import kaantelypeli.fs.FileOperations;
 import org.tinylog.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
+import static kaantelypeli.engine.Entity.SCALE;
 import static kaantelypeli.fs.FileOperations.loadSprite;
 
 public class LevelEditor {
-    public static final int SCALE = 2;
 
     public void editorMenu(Stage stage) {
         ChoiceDialog<Integer> choice = new ChoiceDialog<>();
@@ -41,7 +40,9 @@ public class LevelEditor {
         pane.setOnMouseClicked(c -> {
             int x = (int) c.getSceneX();
             int y = (int) c.getSceneY();
-            pane.getChildren().add(editDialog(floorToScale(x), floorToScale(y), stage));
+            editDialog(floorToScale(x), floorToScale(y), stage).ifPresent(node -> {
+                pane.getChildren().add(node);
+            });
 
             ArrayList<Entity> nodes = new ArrayList<>();
             pane.getChildren().stream().forEach(child -> nodes.add(new Entity(child.getId(),
@@ -63,35 +64,42 @@ public class LevelEditor {
         return (i / (16 * SCALE)) * 16 * SCALE;
     }
 
-    private static Rectangle editDialog(int x, int y, Stage stage) {
-        TextField type = new TextField();
+    private static Optional<Rectangle> editDialog(int x, int y, Stage stage) {
+        ChoiceBox<String> type = new ChoiceBox();
         TextField width = new TextField();
         TextField height = new TextField();
-        type.setPromptText("type");
+        type.setItems(FXCollections.observableArrayList(Arrays.asList(
+                "door", "key", "lava", "player", "victory", "wall")));
+
         width.setPromptText("width");
         height.setPromptText("height");
 
         VBox inputs = new VBox();
         inputs.getChildren().addAll(type, width, height);
 
-        Dialog dialog = new TextInputDialog();
+        TextInputDialog dialog = new TextInputDialog();
         dialog.initOwner(stage);
         dialog.setHeaderText("Editing tile [" + x + "," + y + "]");
         dialog.setContentText("Set tile type");
+        dialog.setGraphic(null);
+        dialog.setTitle(null);
         dialog.getDialogPane().setContent(inputs);
 
         Optional<String> result = dialog.showAndWait();
         Rectangle rect = new Rectangle(x, y, 0, 0);
-        result.ifPresent(input -> {
-            rect.setId(type.getText());
-            rect.setFill(loadSprite(type.getText(), SCALE));
+
+        if (result.isPresent() && !type.getValue().equals("")) {
+            rect.setId(type.getValue());
+            rect.setFill(loadSprite(type.getValue(), SCALE));
             if (width.getText().equals("")) rect.setWidth(16 * SCALE);
             else rect.setWidth(Integer.parseInt(width.getText()) * SCALE);
             if (height.getText().equals("")) rect.setHeight(16 * SCALE);
             else rect.setHeight(Integer.parseInt(height.getText()) * SCALE);
             dialog.close();
-        });
+            return Optional.of(rect);
+        } else {
+            return Optional.empty();
+        }
 
-        return rect;
     }
 }
