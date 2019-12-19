e0bde17c850e0eef81b830a7d162f78702198b42
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-12-19 20:55:31 +0200

    Update fileserver

--- kaantelypeli/src/main/python/fileserver.py
+++ kaantelypeli/src/main/python/fileserver.py
@@ -1,16 +1,25 @@
 from flask import Flask, request
-import flask_fs as fs
+import os
+import json
 
 app = Flask(__name__)
-fs.init_app(app)
+if __name__ == '__main__':
+    if not os.path.exists('levels'):
+        os.makedirs('levels')
+    app.run(debug=True)
 
-@app.route('/levels/<level_id>', methods = ['GET', 'POST'])
+
+@app.route('/levels', methods=['GET'])
+def list_level():
+    return str(os.listdir('levels'))
+
+
+@app.route('/levels/<level_id>', methods=['GET', 'POST'])
 def level(level_id):
-    levels = fs.Storage('levels', 'json', overwrite=True)
     if request.method == 'GET':
-        if levels.exists(id):
-           return levels.read('level_id')
+        with open('levels/' + level_id + '.json', "r") as file:
+            return file.read()
     if request.method == 'POST':
-        levels.write(level_id, request.form)
-    else:
-        return 'invalid request', 405
+        with open('levels/' + level_id + '.json', "w+") as file:
+            file.write(json.dumps(request.form))
+            return "successful upload"
