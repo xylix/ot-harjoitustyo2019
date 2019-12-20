1ae6ad276c4a5b5d8853ece494e36e97072e8313
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-12-20 22:21:22 +0200

    Move semi-consistent LevelEditor tests that require backend to integration test package. Update pom.xml, now automatically make multiplatform .jars

---
+++ kaantelypeli/src/test/java/integration/LevelEditorIT.java
@@ -0,0 +1,126 @@
+package integration;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import kaantelypeli.ui.Game;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.concurrent.TimeoutException;
+
+import static kaantelypeli.ui.Game.FILESERVER;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
+
+public class LevelEditorIT extends ApplicationTest {
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().muteForSuccessfulTests().enableLog();
+
+    @Before
+    public void setUp() throws TimeoutException {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(Game.class);
+    }
+
+    @Test
+    public void addTile() {
+        openFirstLevel();
+        systemOutRule.clearLog();
+        spawnTile();
+        push(KeyCode.ENTER);
+        assertThat(systemOutRule.getLog(), containsString("TRACE: {\"type\":\"wall\""));
+        assertThat(systemOutRule.getLog(), containsString("\"actionMap\":{},\"width\":16,\"height\":16,"));
+    }
+
+
+    @Test
+    public void cancelAddingTile() {
+        openFirstLevel();
+        systemOutRule.clearLog();
+        clickOn(MouseButton.PRIMARY);
+        push(KeyCode.ESCAPE);
+        assertEquals("", systemOutRule.getLog());
+    }
+
+    @Test
+    public void addWideTile() {
+        openFirstLevel();
+        systemOutRule.clearLog();
+        spawnTile();
+        // Set width and height
+        push(KeyCode.TAB);
+        push(KeyCode.DIGIT3);
+        push(KeyCode.DIGIT2);
+        push(KeyCode.TAB);
+        push(KeyCode.DIGIT3);
+        push(KeyCode.DIGIT2);
+
+        push(KeyCode.ENTER);
+        assertThat(systemOutRule.getLog(), containsString(
+                "TRACE: {\"type\":\"wall\""
+        ));
+        assertThat(systemOutRule.getLog(), containsString(",\"width\":32,\"height\":32"));
+    }
+
+    public void spawnTile() {
+        clickOn(MouseButton.PRIMARY);
+        // Navigate to type selector.
+        push(KeyCode.TAB);
+        push(KeyCode.SPACE);
+        push(KeyCode.SPACE);
+    }
+
+    @Test
+    public void levelUpload() {
+        openFirstLevel();
+        push(KeyCode.TAB);
+        push(KeyCode.TAB);
+        push(KeyCode.SPACE);
+        sleep(200);
+        assertThat(systemOutRule.getLog(), containsString("TRACE: POSTing to " + FILESERVER + "/levels/"));
+        assertThat(systemOutRule.getLog(), containsString("TRACE: kong.unirest.EmptyResponse"));
+    }
+
+    @Test
+    public void saveLevel() {
+        openFirstLevel();
+        if (System.getProperty("testfx.headless", "false").equals("true")) {
+            // Skip test if running headless, Monocle's FileChooser dialog is not implemented on headless environments
+            assumeTrue(false);
+        }
+        push(KeyCode.TAB);
+        push(KeyCode.SPACE);
+        sleep(200);
+        push(KeyCode.E);
+        push(KeyCode.ENTER);
+
+        assertThat(systemOutRule.getLog(), containsString("TRACE: Saving file to"));
+    }
+
+    @Test
+    public void cancelledSave () {
+        openFirstLevel();
+        if (System.getProperty("testfx.headless", "false").equals("true")) {
+            // Skip test if running headless, Monocle's FileChooser dialog is not implemented on headless environments
+            assumeTrue(false);
+        }
+        push(KeyCode.TAB);
+        push(KeyCode.SPACE);
+        sleep(200);
+        push(KeyCode.ESCAPE);
+        assertThat(systemOutRule.getLog(), containsString("Save dialog cancelled"));
+    }
+
+    public void openFirstLevel() {
+        clickOn("#editor");
+        push(KeyCode.DOWN);
+        push(KeyCode.ENTER);
+    }
+}
+
--- kaantelypeli/src/test/java/kaantelypeli/ui/LevelEditorTest.java
+++ kaantelypeli/src/test/java/kaantelypeli/ui/LevelEditorTest.java
@@ -1,28 +1,21 @@
 package kaantelypeli.ui;
 
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import java.util.concurrent.TimeoutException;
 
-import static kaantelypeli.ui.Game.FILESERVER;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assume.assumeTrue;
 
 public class LevelEditorTest extends ApplicationTest {
     @Rule
     public final SystemOutRule systemOutRule = new SystemOutRule().muteForSuccessfulTests().enableLog();
-    @Rule
-    public final SystemErrRule systemErrRule = new SystemErrRule().muteForSuccessfulTests();
 
     @Before
     public void setUp() throws TimeoutException {
@@ -32,101 +25,10 @@ public class LevelEditorTest extends ApplicationTest {
 
     @Test
     public void openLevel() {
-        openFirstLevel();
-        assertThat(systemOutRule.getLog(),
-                containsString("TRACE: Opened a copy of level 1 in editor"));
-    }
-
-    @Test
-    public void addTile() {
-        openFirstLevel();
-        systemOutRule.clearLog();
-        spawnTile();
-        push(KeyCode.ENTER);
-        assertEquals("TRACE: {\"type\":\"wall\",\"x\":0,\"y\":32,\"actionMap\":{},\"width\":16,\"height\":16,\"movable\":false,\"passable\":false}"
-                + System.lineSeparator(), systemOutRule.getLog());
-    }
-
-    @Test
-    public void cancelAddingTile() {
-        openFirstLevel();
-        systemOutRule.clearLog();
-        clickOn(MouseButton.PRIMARY);
-        push(KeyCode.ESCAPE);
-        assertEquals("", systemOutRule.getLog());
-    }
-
-    @Test
-    public void addWideTile() {
-        openFirstLevel();
-        systemOutRule.clearLog();
-        spawnTile();
-        // Set width and height
-        push(KeyCode.TAB);
-        push(KeyCode.DIGIT3);
-        push(KeyCode.DIGIT2);
-        push(KeyCode.TAB);
-        push(KeyCode.DIGIT3);
-        push(KeyCode.DIGIT2);
-
-        push(KeyCode.ENTER);
-        assertEquals("TRACE: {\"type\":\"wall\",\"x\":0,\"y\":32,\"actionMap\":{},\"width\":32,\"height\":32,\"movable\":false,\"passable\":false}"
-                 + System.lineSeparator(), systemOutRule.getLog());
-    }
-    
-    @Test
-    public void saveLevel() {
-        openFirstLevel();
-        if (System.getProperty("testfx.headless", "false").equals("true")) {
-            // Skip test if running headless, Monocle's FileChooser dialog is not implemented on headless environments
-            assumeTrue(false);
-        }
-        push(KeyCode.TAB);
-        push(KeyCode.SPACE);
-        sleep(200);
-        push(KeyCode.E);
-        push(KeyCode.ENTER);
-
-        assertThat(systemOutRule.getLog(), containsString("TRACE: Saving file to"));
-    }
-
-    @Test
-    public void cancelledSave () {
-        openFirstLevel();
-        if (System.getProperty("testfx.headless", "false").equals("true")) {
-            // Skip test if running headless, Monocle's FileChooser dialog is not implemented on headless environments
-            assumeTrue(false);
-        }
-        push(KeyCode.TAB);
-        push(KeyCode.SPACE);
-        sleep(200);
-        push(KeyCode.ESCAPE);
-        assertThat(systemOutRule.getLog(), containsString("Save dialog cancelled"));
-    }
-
-    @Test
-    public void levelUpload() {
-        openFirstLevel();
-        push(KeyCode.TAB);
-        push(KeyCode.TAB);
-        push(KeyCode.SPACE);
-        sleep(200);
-        assertThat(systemOutRule.getLog(), containsString("TRACE: POSTing to " + FILESERVER + "/levels/"));
-        assertThat(systemOutRule.getLog(), containsString("TRACE: kong.unirest.EmptyResponse"));
-    }
-
-    public void spawnTile() {
-        clickOn(MouseButton.PRIMARY);
-        // Navigate to type selector.
-        push(KeyCode.TAB);
-        push(KeyCode.SPACE);
-        push(KeyCode.SPACE);
-    }
-
-    public void openFirstLevel() {
         clickOn("#editor");
-
         push(KeyCode.DOWN);
         push(KeyCode.ENTER);
+        assertThat(systemOutRule.getLog(),
+                containsString("TRACE: Opened a copy of level 1 in editor"));
     }
 }
